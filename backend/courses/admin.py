d4ec7ce9acbaabed00470e6ca9e4ddec62f53164
DamirTB <talipdamir@gmail.com>
2024-10-29 20:10:35 +0500

    some changes

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Quiz, Question
 
+admin.site.register(Course)
+admin.site.register(Quiz)
+admin.site.register(Question)
 
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "duration")
+# @admin.register(Course)
+# class CourseAdmin(admin.ModelAdmin):
+#     list_display = ("title", "description", "duration")
+
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -9,3 +9,28 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=255)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Quiz"
+        verbose_name_plural = 'Quizes'
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    quiz = models.ForeignKey(Quiz, on_delete=models.SET_NULL, null=True)
+    option_a = models.CharField(max_length=255)
+    option_b = models.CharField(max_length=255)
+    option_c = models.CharField(max_length=255)
+    option_d = models.CharField(max_length=255)
+    score_a = models.IntegerField(default=0)
+    score_b = models.IntegerField(default=0)
+    score_c = models.IntegerField(default=0)
+    score_d = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
